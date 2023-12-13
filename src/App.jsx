9aa86d5c3c9b7cfdb50949e8fe65206eefd09e42
Otavio Henrique de Lima e Silva <otaviohlsilva@gmail.com>
2023-12-13 12:48:25 -0300

    commit 1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Banner from './components/Banner'
+import CampoTexto from './components/CampoTexto'
+
+function App() {
+  return (
+    <>
+      <Banner />
+      <CampoTexto />
+    </>
+  )
+}
+
+export default App
